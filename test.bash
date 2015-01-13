2d76d123ac5b29f0473b3bcc0a2cfa7b29b8cf3e
Friedrich Paetzke <paetzke@fastmail.fm>
2015-01-13 21:35:39 +0100

    Add tests

--- test.bash
+++ test.bash
@@ -1,9 +1,15 @@
 #! /bin/bash
 
+
+die() {
+    echo "Error: $1 failed."
+    exit 1
+}
+
+
 install_emacs24() {
     sudo add-apt-repository ppa:cassou/emacs -y
     sudo apt-get update -y
-
     sudo apt-get install emacs24 -y
 }
 
@@ -17,7 +23,7 @@ test_01() {
     emacs -nw ./test_data/test_01/before.sql -f format-sql-buffer -f save-buffer -f save-buffers-kill-terminal
     diff ./test_data/test_01/before.sql ./test_data/test_01/after.sql
     if [ $? != 0 ]; then
-        exit 1
+        die test_01
     fi
 }
 
@@ -26,7 +32,17 @@ test_02() {
     emacs -nw ./test_data/test_02/before.py -f format-sql-buffer -f save-buffer -f save-buffers-kill-terminal
     diff ./test_data/test_02/before.py ./test_data/test_02/after.py
     if [ $? != 0 ]; then
-        exit 1
+        die test_02
+    fi
+}
+
+
+test_03() {
+    emacs -nw ./test_data/test_03/before.not_sql -f format-sql-buffer -f save-buffer -f save-buffers-kill-terminal
+    diff ./test_data/test_03/before.not_sql ./test_data/test_03/after.not_sql
+    if [ $? != 0 ]; then
+        echo
+        die test_03
     fi
 }
 
@@ -39,6 +55,7 @@ main() {
 
     test_01
     test_02
+    test_03
 }
 
 
